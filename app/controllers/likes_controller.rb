fdc2e41250525be78b29f823edfd4b4406767052
Kokichi.Omi <Kokichi.Omi@rpacommunity.com>
2018-09-17 13:28:41 +0000

    Likes fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,14 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    micropost = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success] = "お気に入り登録しました"
     redirect_to root_path
   end
 
   def destroy
-    micropost = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success] = 'お気に入り解除しました'
     redirect_to root_path
